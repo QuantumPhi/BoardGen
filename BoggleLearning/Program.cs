850c068144e2ac78d3054657a4e38f4f42a01083
Tarik Onalan <phi.quantum@gmail.com>
2014-11-11 10:34:15 -0800

    Initial commit

---
+++ BoggleLearning/Program.cs
@@ -0,0 +1,27 @@
+﻿using Accord.Neuro;
+using Accord.Neuro.Learning;
+using Accord.Neuro.Networks;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BoggleLearning
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DeepBeliefNetwork network = new DeepBeliefNetwork(2, 23, 1);
+            new NguyenWidrow(network).Randomize();
+            ParallelResilientBackpropagationLearning learning = new ParallelResilientBackpropagationLearning(network);
+
+            string[] trainingData =
+            {
+                "elotsopestidisfs",
+
+            };
+        }
+    }
+}

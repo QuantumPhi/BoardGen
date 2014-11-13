ea16208aaffe715b0b464c96211e687cc10ea778
Tarik Onalan <phi.quantum@gmail.com>
2014-11-13 14:02:17 -0800

    Need to find a better way to train

--- BoggleLearning/Program.cs
+++ BoggleLearning/Program.cs
@@ -3,14 +3,18 @@ using Accord.Neuro.Learning;
 using Accord.Neuro.Networks;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace BoggleLearning
 {
     class Program
     {
+        readonly static int EPOCHS = 150;
+
         static void Main(string[] args)
         {
             DeepBeliefNetwork network = new DeepBeliefNetwork(2, 23, 1);
@@ -20,8 +24,31 @@ namespace BoggleLearning
             string[] trainingData =
             {
                 "elotsopestidisfs",
-
             };
+
+            var input = GetCharTypes(trainingData).ToArray();
+            var output = new double[][]
+            {
+                new double[] {1}
+            };
+
+            for (int i = 0; i < EPOCHS; i++)
+                Console.WriteLine(learning.RunEpoch(input, output));
+
+            Debugger.Break();
+        }
+
+        static IEnumerable<double[]> GetCharTypes(string[] data)
+        {
+            int vowels = 0,
+                consonants = 0;
+
+            foreach (string item in data)
+            {
+                vowels = Regex.Matches(item.ToLower(), @"[aeiou]").Count;
+                consonants = item.Length - vowels;
+                yield return new double[] { vowels, consonants };
+            }
         }
     }
 }

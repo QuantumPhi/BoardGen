cba182358124d235cdf7d8e089af09936a47e377
Tarik Onalan <phi.quantum@gmail.com>
2014-12-24 00:23:07 -0800

    Not brute force at all...

---
+++ BoardGen/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BoardGen
+{
+    class Program
+    {
+        static readonly Random random = new Random();
+
+        static void Main(string[] args)
+        {
+            string letters = "abcdefghijklmnopqrstuvwxyz";
+            string board = "";
+
+            for(int i = 0; i < 16; i++)
+            {
+                board += letters[random.Next(letters.Length)];
+            }
+
+            Console.WriteLine(board);
+        }
+    }
+}
